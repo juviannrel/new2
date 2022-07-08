fec74e21a26f5e248dadeb5c445aca36fab3d7d7
juviannrel <106714202+juviannrel@users.noreply.github.com>
2022-07-08 19:15:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 let fs = require('fs')                    //CREATOR = YahyaGanzz , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6281511480762'],
-  ['66281511480762'],
-  ['6281511480762', 'Owner AzzBot', true]  
+  ['6282362285077'],
+  ['6285814772390'],
+  ['6282362285077', 'Owner Juvian', true]  
   
 ] 
-global.ownername = [['YahyaGanzz']]
-global.namabot = [['AzzBot-MD']]
-global.ownerinsta = [['@wibu_indo_888816']]
-global.mods = [] 
-global.prems = [] 
+global.ownername = [['JUVIANN']]
+global.namabot = [['VIANN-MD']]
+global.ownerinsta = [['@tarunatam00']]
+global.mods = [['6282362285077','6285814772390']] 
+global.prems = [['6282362285077']] 
 
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'AzzBot-MD'
-  var sticker_author = 'By YahyaGanzz'
+  var sticker_name = 'Viann-MD'
+  var sticker_author = 'By JuviannCool'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -61,7 +61,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Created by YahyaGanzz'
+global.wm = 'Created by Juviann'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
